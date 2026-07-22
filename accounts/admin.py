be96c7cc6776070d3750cb498be475db0062e60b
aman-ranjan-1 <aman.ranjan21a@gmail.com>
2026-07-22 14:48:51 +0530

    Implement authentication and core backend modules

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import PatientProfile, DoctorProfile
 
-# Register your models here.
+@admin.register(PatientProfile)
+class PatientProfileAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "id",
+        "user",
+        "created_at",
+    )
+
+    search_fields = (
+        "user__username",
+        "user__email",
+    )
+@admin.register(DoctorProfile)
+class DoctorProfileAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "id",
+        "user",
+        "specialization",
+        "created_at",
+    )
+
+    search_fields = (
+        "user__username",
+        "user__email",
+        "specialization",
+    )
